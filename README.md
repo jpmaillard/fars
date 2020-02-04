e22852fa970536b3e4bcd579626cc91104ef51c4
Jean-Philippe Maillard <maillardjp@gmail.com>
2020-02-04 14:04:46 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# fars Package
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of fars is, as part of the John Hopkins Coursera course: Mastering R Pacakge Develoipment, to demonstrate how to create a package, run tests and get a travis badge.
+
+
+## The package
+
+You can see the release of this fars version from [github/jpmaillard/fars](https://github.com/jpmaillard/fars) 
---
+++ tests/testthat/test_functions.R
@@ -0,0 +1,18 @@
+context("Test the basic functionality of the package")
+
+library(dplyr)
+library(maps)
+
+setwd(system.file("extdata", package = "fars"))
+
+test_that("fars_read() works correctly", {
+  expect_is(fars_read("accident_2015.csv.bz2"), "tbl_df")
+  expect_error(fars_read("accident_2020.csv.bz2"))
+})
+
+
+test_that("fars_summarize_years() works correctly", {
+  expect_is(fars_summarize_years(list(2013, 2014)), "tbl_df")
+  expect_error(fars_summarize_years(2016))
+
+})
